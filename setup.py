e88e0d87f4ea2271526e9458f8356c1224e7a2a2
HazemBZ <hazembenbz@gmail.com>
2023-11-06 15:57:57 +0100

    chore: Update setup configs

--- setup.py
+++ setup.py
@@ -12,19 +12,19 @@ with open('README.rst', 'r') as r:
     readme = r.read()
 
 download_url = (
-    'https://github.com/juliomalegria/django-chunked-upload/tarball/%s'
+    'https://github.com/HazemBZ/django-file-nunckak/tarball/%s'
 )
 
 setup(
-    name='django-chunked-upload',
+    name='django-file-nunckak',
     packages=['chunked_upload', 'chunked_upload.migrations', 'chunked_upload.management'],
     version=version,
     description=('Upload large files to Django in multiple chunks, with the '
                  'ability to resume if the upload is interrupted.'),
     long_description=readme,
-    author='Julio M Alegria',
-    author_email='juliomalegria@gmail.com',
-    url='https://github.com/juliomalegria/django-chunked-upload',
+    author='Hazem B Bouzaine',
+    author_email='hazembenbz@gmail.com',
+    url='https://github.com/HazemBZ/django-file-nunckak',
     download_url=download_url % version,
     install_requires=[],
     license='MIT-Zero'
